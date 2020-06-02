4f7969972c9f322c62b86b67eafcde320fc6ce0d
zhoushengjie <zhou_shengjie@outlook.com>
2020-06-02 14:33:15 +0800

    log 增加 pos

--- log/log.go
+++ log/log.go
@@ -1,28 +1,51 @@
 package log
 
 import (
+	"fmt"
+
 	"github.com/shunjiecloud/errors"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var defaultLogger *zap.Logger
 
 func init() {
 	var err error
-	defaultLogger, err = zap.NewProduction()
+	config := zap.NewProductionConfig()
+	config.DisableCaller = true
+	config.DisableStacktrace = true
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	defaultLogger, err = config.Build()
 	if err != nil {
 		panic(err)
 	}
 }
 
+func pos(err *errors.Error) {
+	//  add pos into fields
+	pos := make([]string, 0)
+	for _, f := range err.StackFrames() {
+		pos = append(pos, fmt.Sprintf("%v %v", f.File, f.LineNumber))
+	}
+	err.Fields = append(err.Fields, zap.Strings("pos", pos))
+}
+
 func Warn(err *errors.Error) {
+	//  pos
+	pos(err)
 	defaultLogger.Warn(err.Error(), err.Fields...)
 }
 
 func Error(err *errors.Error) {
+	//  pos
+	pos(err)
 	defaultLogger.Error(err.Error(), err.Fields...)
 }
 
 func Info(err *errors.Error) {
+	//  pos
+	pos(err)
 	defaultLogger.Info(err.Error(), err.Fields...)
 }
